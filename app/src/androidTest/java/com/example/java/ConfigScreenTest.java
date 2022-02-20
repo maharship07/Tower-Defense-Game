328946b88dacc7fd59e77fe7f4408ae9f7246f4e
Manu Gupta <mgupta@lawn-128-61-116-9.lawn.gatech.edu>
2022-02-20 13:48:50 -0500

    Fixed tests and added starting money

--- app/src/androidTest/java/com/example/java/ConfigScreenTest.java
+++ app/src/androidTest/java/com/example/java/ConfigScreenTest.java
@@ -1,6 +1,7 @@
 package com.example.java;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -8,10 +9,9 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import android.app.Activity;
 import android.content.Context;
-
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
 //import org.junit.Before;
 import org.junit.Rule;
@@ -30,30 +30,37 @@ public class ConfigScreenTest  {
     private Activity mActivity;
 
     @Rule
-    public ActivityScenarioRule<ConfigScreen> activityRule =
-            new ActivityScenarioRule<ConfigScreen>(ConfigScreen.class);
+    public ActivityTestRule<ConfigScreen> activityRule =
+            new ActivityTestRule(ConfigScreen.class);
 
     @Test
     public void testNormalName() {
-        onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.playerName)).perform(typeText("Yay"));
+        onView(withId(R.id.playerName)).perform(closeSoftKeyboard());
+
         onView(withId(R.id.normalButton)).perform(click());
+        onView(withId(R.id.startGame)).perform(click());
         onView(withId(R.id.healthCounter)).check(matches(isDisplayed()));
+
     }
 
     @Test
     public void testBlankName() {
-        onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.playerName)).perform(typeText(""));
+        onView(withId(R.id.playerName)).perform(closeSoftKeyboard());
+
         onView(withId(R.id.normalButton)).perform(click());
+        onView(withId(R.id.startGame)).perform(click());
         onView(withId(R.id.normalButton)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testWhiteSpaceName() {
-        onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.playerName)).perform(typeText("   "));
+        onView(withId(R.id.playerName)).perform(closeSoftKeyboard());
+
         onView(withId(R.id.normalButton)).perform(click());
+        onView(withId(R.id.startGame)).perform(click());
         onView(withId(R.id.normalButton)).check(matches(isDisplayed()));
     }
 
--- app/src/main/java/com/example/java/GameScreen.java
+++ app/src/main/java/com/example/java/GameScreen.java
@@ -7,11 +7,16 @@ import android.widget.TextView;
 
 public class GameScreen extends Activity {
     private int health; //Player health
+    private int money;
 
     @SuppressLint("SetTextI18n")
-    //Takes in health text field and sets it to current health value
     private void updateHealth(TextView healthCounter) {
         healthCounter.setText("Health: " + health);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateMoney(TextView moneyCounter) {
+        moneyCounter.setText("Money: " + money);
     }
 
     @Override
@@ -19,21 +24,26 @@ public class GameScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_screen);
         TextView healthCounter = findViewById(R.id.healthCounter); //Initializes health display
+        TextView moneyCounter = findViewById(R.id.moneyCounter);
         Bundle extras = getIntent().getExtras(); //Pulls all variables passed from config screen
         int diff = extras.getInt("diff"); // Pulls difficulty from config screen
         switch (diff) { //initializes game parameters based on difficulty parameter
         case 0:
             health = 150; //Easy health = 150
+            money = 200;
             break;
         case 1:
             health = 100; //Normal health = 100
+            money = 150;
             break;
         case 2:
             health = 50; //Hard health = 50
+            money = 100;
             break;
         default:
             throw new IllegalStateException("Unexpected value: " + diff);
         }
         updateHealth(healthCounter); //sets health display to starting health
+        updateMoney(moneyCounter);
     }
 }
